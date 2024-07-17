ab8e6fc648cd23a8b5ce06a6d26b09ea021b19d6
gitcrusher <aayushguptasoni@gmail.com>
2024-07-17 21:24:27 +0530

    commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,9 +4,9 @@ function Card() {
 
     const data = [
         {image: "https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww",
-            name: "Amazon Basics", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam ut aut adipisci autem maxime corporis ullam quidem numquam! Nemo, inventore magnam."},
-        {image: "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG9sYSUyMGNvbXBhbnl8ZW58MHx8MHx8fDA%3D", name:'Ola', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam ut aut adipisci autem '},
-        {image: "https://images.unsplash.com/photo-1565531152238-5f20a0f4a3f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8VWJlciUyMGNvbXBhbnl8ZW58MHx8MHx8fDA%3D", name:'Uber', description:'Lorem ipsum dolor sit amet consectetur adipisicing elitFacilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam'}
+            name: "Amazon Basics", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam ut aut adipisci autem maxime corporis ullam quidem numquam! Nemo, inventore magnam.",instock:true},
+        {image: "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG9sYSUyMGNvbXBhbnl8ZW58MHx8MHx8fDA%3D", name:'Ola', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam ut aut adipisci autem ',instock:false},
+        {image: "https://images.unsplash.com/photo-1565531152238-5f20a0f4a3f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8VWJlciUyMGNvbXBhbnl8ZW58MHx8MHx8fDA%3D", name:'Uber', description:'Lorem ipsum dolor sit amet consectetur adipisicing elitFacilis vel, explicabo pariatur iusto voluptates possimus inventore nemo quisquam',instock:false},
     ]
 
     return (
@@ -25,7 +25,9 @@ function Card() {
                     <p className='text-xs mt-3'>
                         {elem.description}
                     </p>
-                    <button className='px-4 py-2 bg-blue-400 text-xs rounded-full font-semibold border-cyan-200  border-x-2 border-y-2 text-zinc-100 mt-3'>Know more</button>
+                    <button className={`px-4 py-2 ${elem.instock ? 'bg-blue-600 border-cyan-200':'bg-red-600 border-red-400' } text-xs rounded-lg font-semibold   border-x-4 border-y-4 text-zinc-100 mt-3`}>
+                        {elem.instock ?"In stock":"Out of stock"}
+                    </button>
                 </div>
             </div>
             ))}
