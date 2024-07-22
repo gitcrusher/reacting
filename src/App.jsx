77774d53dc675af536a9456e36a029fab083ef84
gitcrusher <aayushguptasoni@gmail.com>
2024-07-23 00:59:56 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Card from "./Components/Card";
-import Song from "./Components/Song";
+import React, { useState } from 'react'
 
-function abcd(){
+function App() {
+  const[val,setVal] = useState({name: "harsh", isBanned: false});
   return (
-    <>
-        <Card/>
-        <Song/>
-    </>
-);
-  };
+    <div className='p-4'>
+      <h1 className='px-2 text-black'>name: {val.name}</h1>
+      <h2 className='px-2'>banned : {val.isBanned.toString()}</h2>
+      <button onClick={()=> setVal({...val, isBanned:!val.isBanned  })} className={`px-4 py-1 ${val.isBanned ? "bg-blue-600":"bg-red-600"} text-white text-xs rounded-full`}>Change</button>
+    </div>
+  )
+}
 
-export default abcd;
+export default App
