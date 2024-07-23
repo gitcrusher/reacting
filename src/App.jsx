40c93dc040c91706836441538b4b2345252ae169
gitcrusher <aayushguptasoni@gmail.com>
2024-07-23 10:05:23 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 
 function App() {
-  const[val,setVal] = useState({name: "harsh", isBanned: false});
+  const [val,setVal] = useState({name: "haresh", age: 24});
   return (
-    <div className='p-4'>
-      <h1 className='px-2 text-black'>name: {val.name}</h1>
-      <h2 className='px-2'>banned : {val.isBanned.toString()}</h2>
-      <button onClick={()=> setVal({...val, isBanned:!val.isBanned  })} className={`px-4 py-1 ${val.isBanned ? "bg-blue-600":"bg-red-600"} text-white text-xs rounded-full`}>Change</button>
+    <div>
+    <button onClick={()=>{setVal({...val,gender: "male"});
+  console.log(val)}}>click</button>
     </div>
   )
 }
